8840f4b2818129879e3e427f02d5f846c3a0bee7
vinayak <vinayakanvi2000@gmail.com>
2025-03-13 15:24:30 +0530

    Defect-3456-logine-error-closed

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test3Application {
 
+
 	public static void main(String[] args) {
+          int x = 100;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
